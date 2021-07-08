92797ef187acf0349be91908041792df104fd6d0
jordan.dainese <jordan.dainese@siemens.com>
2021-07-08 14:24:15 +0200

    plz validation

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -16,14 +16,18 @@ export class FormularComponent implements OnInit {
   onsubmit(username: string, name: string, nachname: string, email: string, adresse: string, plz: string, ort: string): void {
     // @ts-ignore
     document.getElementById("anmeldeForm").submit;
-    // let formsIsValid = true;
+    let formsIsValid: boolean = true;
     // if (username != null) {
     //   if (!/[^a-zA-Z]/.test(username)) {
     //     alert(username)
     //   }
     // }
 
-
+    let numberPLZ: number = +plz;
+    if (numberPLZ > 9999 || numberPLZ < 1000) {
+      formsIsValid = false;
+      alert("Die Postleitzahl ist nicht 4-Stellig")
+    }
 
   }
 
