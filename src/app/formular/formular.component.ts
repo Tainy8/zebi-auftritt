680312913af7d0184f2cae3544eff6c72cbaaf3b
jordan.dainese <jordan.dainese@siemens.com>
2021-07-09 10:33:25 +0200

    some valdi sheit

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -19,20 +19,30 @@ export class FormularComponent implements OnInit {
 
   onsubmit(username: string, name: string, nachname: string, email: string, adresse: string, plz: string, ort: string): void {
     // @ts-ignore
-    document.getElementById("anmeldeForm").submit;
     let formsIsValid: boolean = true;
-    // if (username != null) {
-    //   if (!/[^a-zA-Z]/.test(username)) {
-    //     alert(username)
-    //   }
-    // }
+
+    let reName = /[^a-z]/i;
+    if (reName.test(name)) {
+      alert("Der Eingegeben Vorname darf nur aus Buchstaben");
+      formsIsValid = false;
+    }
+
+    if (reName.test(nachname)) {
+      alert("Der Eingegeben Nachname darf nur aus Buchstaben");
+      formsIsValid = false;
+    }
 
     let numberPLZ: number = +plz;
     if (numberPLZ > 9999 || numberPLZ < 1000) {
       formsIsValid = false;
-      alert("Die Postleitzahl ist nicht 4-Stellig")
+      // alert("Die Postleitzahl ist nicht 4-Stellig");
     }
 
+    const reEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!reEmail.test(email.toLowerCase())) {
+      //alert("Bitte gib eine valide Emailadresse ein");
+      formsIsValid = false;
+    }
   }
 
 }
